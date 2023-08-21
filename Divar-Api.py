9b565604b681baec0a8e6f61c1cf042a347e6da0
Mohammad Sadegh Majidi Kadkani <74909796+Cmatrix1@users.noreply.github.com>
2023-08-21 15:47:33 +0330

    Update Divar-Api.py
    refactor: make better the code

--- Divar-Api.py
+++ Divar-Api.py
@@ -1,70 +1,76 @@
-from requests import post, get
+
+from requests import cookies, post, get
 from unidecode import unidecode
 from time import sleep
 
-YourToken = 'Basic ' ## Enter your token from Cookies below
-
-
 class Divar:
-    def __init__(self, city="tehran", category="real-estate", cnt=10):
-        self.city = city
-        self.category = category
-        self.pro_cnt = cnt
+    def __init__(self, headers: dict = None, cookies: dict = None):
         self.url = "https://api.divar.ir/v8/web-search/3/"
-        self.ext_lst = []
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Referer': 'https://divar.ir/',
-            'Origin': 'https://divar.ir',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-site',
-            'Authorization': YourToken,
-            'Connection': 'keep-alive',
+        self.headers = headers or {}
+        self.cookies = cookies or {}
+
+    def send_request(self, 
+                     category: str = "light", 
+                     last_post_date: int = 0, 
+                     min_price: int = 0,
+                     query: str = ""):
+        json = {
+            "json_schema": {
+                "category": {
+                    "value": category
+                },
+                "cities": [
+                    "3"
+                ],
+                "price": {
+                    "min": min_price
+                },
+                "query": query
+            },
+            "last-post-date": last_post_date,
+            "page": 1
         }
+        return post(self.url+category, json=json).json()
 
-    def __send_request(self, url, last_post_date=0):
-        json = {'json_schema': {'category': {'value': self.category}}, 'last-post-date': last_post_date}
-        return post(url+self.category, json=json).json()
-    
-    def __product_detail(self, product):
+    def get_product_detail(self, product_data):
         return {
-                'token': product['data']['token'],
-                'title': product['data']['title'],
-                'image': product['data']['image_url'],
-                'price': unidecode(product['data']['middle_description_text'][:-6]),
-                'description': product['data']['bottom_description_text']}
+            'token': product_data['token'],
+            'title': product_data['title'],
+            'image': product_data['image_url'],
+            'price': unidecode(product_data['middle_description_text'][:-6]),
+            'description': product_data['bottom_description_text']
+            }
 
-    def extract_prodcuts(self, content):
-        for product in content["web_widgets"]["post_list"]:
-            self.ext_lst.append(self.__product_detail(product))
-            if len(self.ext_lst) == self.pro_cnt:
-                return False
-        return True
-    
-    def extract_number(self, token):
-        response = get(f'https://api.divar.ir/v8/postcontact/web/contact_info/{token}', headers=self.headers).json()
+    def get_prodcuts(self, 
+                     category: str = "ROOT", 
+                     last_post_date: int = 0, 
+                     min_price: int = 0,
+                     query: str = ""):
+        
+        response = self.send_request(category, last_post_date, min_price, query)
+        for product in response["web_widgets"].get("post_list", []):
+            yield self.get_product_detail(product["data"])
+
+    def get_all_products(self, 
+                     category: str = "ROOT", 
+                     last_post_date: int = 0, 
+                     min_price: int = 0,
+                     query: str = ""):
+        
+        response = self.send_request(category, last_post_date, min_price, query)
+        while last_post_date != None:
+            for product in response["web_widgets"].get("post_list", []):
+                yield self.get_product_detail(product["data"])
+
+            last_post_date = response["last_post_date"]
+            response = self.send_request(category, last_post_date, min_price, query)
+
+    def get_number(self, post_token):
+        response = get(
+            f'https://api.divar.ir/v8/postcontact/web/contact_info/{post_token}', headers=self.headers).json()
         try:
             return response["widget_list"][0]["data"]["action"]["payload"]["phone_number"]
-        except:
-            None
-
-    def main(self):
-        req = self.__send_request(self.url)
-        status = True
-        while status:
-            status = self.extract_prodcuts(req)
-            req = self.__send_request(self.url, req["last_post_date"])
-            sleep(2)
-        return self.ext_lst
-
-divar = Divar("mashhad", cnt=20)
-
-## For Extract Numbers
-for product in divar.main():
-    number = divar.extract_number(product["token"])
-    print(number)
+        except KeyError:
+            return None
 
 
